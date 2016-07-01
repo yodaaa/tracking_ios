ce6b1a5c46943f198a46e8b902de71e7ad65b66e
yodaaa <01ewell0@gmail.com>
2016-07-01 14:53:08 +0900

    Initial Commit

---
+++ googlemap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  googlemap
+//
+//  Created by 依田真明 on 2016/07/01.
+//  Copyright © 2016年 MasaakiYoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
